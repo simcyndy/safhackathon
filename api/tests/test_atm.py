3aa1c6451c12f65dfba8ebb33722dcf069f5a2fd
unknown <cynthia.simiyu@globalfinanceplan.com>
2019-06-21 11:04:28 +0300

    Update

--- api/tests/test_atm.py
+++ api/tests/test_atm.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 
-import pytest
-import mock
-# from mixer.backend.django import mixer
+import unittest
+
+from mock import patch
+from unittest import TestCase
 from api.backend.interfaces import atm
 
-# noinspection SpellCheckingInspection
-pytestmark = pytest.mark.django_db
 
-
-def test_check_account():
-	with mock.patch.object(__builtins__, 'input', lambda: 'B'):
-		assert atm.check_account() == 'expected_output'
+class TestAnswer(TestCase):
+	def test_balance_return_val(self):
+		with patch('__builtin__.raw_input', return_value = 'B') as _raw_input:
+			self.assertEqual(atm.check_account(), 'Your current account balance is : 0.00')
+			_raw_input.assert_called_once_with('Enter a character to perform an operation:')
